eef41854bd345746855e2a8fce7eb55ab9771edd
Hun-0322 <ksh26271@naver.com>
2022-12-23 17:50:25 +0900

    Networking 콜백함수 설계

--- (ex)ItunesAPI.playground/Contents.swift
+++ (ex)ItunesAPI.playground/Contents.swift
@@ -32,11 +32,12 @@ struct Music: Codable {
 }
 
 
-func getMethod() {
+func getMethod(completion: @escaping ([Music]?) -> Void) {
 
     // URL구조체 만들기
     guard let url = URL(string: "https://itunes.apple.com/search?media=music&term=k-pop") else {
         print("Error: cannot create URL")
+        completion(nil)
         return
     }
     
@@ -44,38 +45,42 @@ func getMethod() {
     var request = URLRequest(url: url)
     request.httpMethod = "GET"
     
-    
     // 요청을 가지고 작업세션시작
-    
+    // 오래걸리는 작업 -> 그래서 내부에 비동기 처리 되어있음. 그래서 함수를 리턴형으로 설계하면 무조건 nil이 나온다. 즉, 콜백함수로 구현해야함.
     URLSession.shared.dataTask(with: request) { data, response, error in
         // 에러가 없어야 넘어감
         guard error == nil else {
             print("Error: error calling GET")
             print(error!)
+            completion(nil)
             return
         }
         // 옵셔널 바인딩
         guard let safeData = data else {
             print("Error: Did not receive data")
+            completion(nil)
             return
         }
         // HTTP 200번대 정상코드인 경우만 다음 코드로 넘어감
         guard let response = response as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
             print("Error: HTTP request failed")
+            completion(nil)
             return
         }
-
-        // 원하는 모델이 있다면, JSONDecoder로 decode코드로 구현 ⭐️
         do {
             let decoder = JSONDecoder()
-            let musicArray = try decoder.decode(MusicData.self, from: safeData)
-            dump(musicArray)
+            let musicData = try decoder.decode(MusicData.self, from: safeData)
+            completion(musicData.results)
+            return
         } catch {
             
         }
-
-
+        
     }.resume()     // 시작
 }
 
-getMethod()
+getMethod { musicArray in
+    guard let array = musicArray else { return }
+    dump(array)
+}
+
